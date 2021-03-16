239acf449f9da0f1c2d4de2c1b112604e6118c2d
kene Igweagu <kodigweagu@gmail.com>
2021-03-15 21:23:06 -0400

    Use bootstrap grid instead of tables

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,61 +1,61 @@
 app.controller('MainController', ['$scope', '$http', function($scope, $http){
-	
-	$scope.LIST_SIZE = 10;
+    
+    $scope.LIST_SIZE = 10;
 
-	$scope.orderOptions = [
-	{ display: "Total Confirmed", value: "TotalConfirmed"},
-	{ display: "New Confirmed", value: "NewConfirmed"},
-	{ display: "Total Deaths", value: "TotalDeaths"},
-	{ display: "New Deaths", value: "NewDeaths"},
-	{ display: "Total Recovered", value: "TotalRecovered"},
-	{ display: "New Recovered", value: "NewRecovered"}
-	];
+    $scope.orderOptions = [
+    { display: "Total Confirmed", value: "TotalConfirmed"},
+    { display: "New Confirmed", value: "NewConfirmed"},
+    { display: "Total Deaths", value: "TotalDeaths"},
+    { display: "New Deaths", value: "NewDeaths"},
+    { display: "Total Recovered", value: "TotalRecovered"},
+    { display: "New Recovered", value: "NewRecovered"}
+    ];
 
-	$scope.orderBy = "TotalConfirmed";
+    $scope.orderBy = "TotalConfirmed";
 
-	$scope.showCountry = function(slug){
-		$scope.country = $scope.data.Countries.filter(function(element){ return element.Slug == slug;})[0];
-	};
+    $scope.showCountry = function(slug){
+        $scope.country = $scope.data.Countries.filter(function(element){ return element.Slug == slug;})[0];
+    };
 
-	$scope.showGlobal = function(){
-		$scope.country = $scope.Global;
-	};
+    $scope.showGlobal = function(){
+        $scope.country = $scope.Global;
+    };
 
-	$scope.showMore = function(){
-		$scope.LIST_SIZE = $scope.data.Countries.length;
-		document.getElementById("show-more").style.display = "none";
-		document.getElementById("show-less").style.display = "";
-	};
+    $scope.showMore = function(){
+        $scope.LIST_SIZE = $scope.data.Countries.length;
+        document.getElementById("show-more").style.display = "none";
+        document.getElementById("show-less").style.display = "";
+    };
 
-	$scope.showLess = function(){
-		$scope.LIST_SIZE = 10;
-		document.getElementById("show-less").style.display = "none";
-		document.getElementById("show-more").style.display = "";
-	};
+    $scope.showLess = function(){
+        $scope.LIST_SIZE = 10;
+        document.getElementById("show-less").style.display = "none";
+        document.getElementById("show-more").style.display = "";
+    };
 
-	$scope.getSummary = function(){
-		$http({
-			method: 'GET',
-			url: 'https://api.covid19api.com/summary'
-		}).then(function (response) {
-			$scope.Global = {
-				"ID": response.data.ID,
-				"Country": "Worldwide",
-				"CountryCode": "WW",
-				"Slug": "world",
-				"NewConfirmed": response.data.Global.NewConfirmed,
-				"TotalConfirmed": response.data.Global.TotalConfirmed,
-				"NewDeaths": response.data.Global.NewDeaths,
-				"TotalDeaths": response.data.Global.TotalDeaths,
-				"NewRecovered": response.data.Global.NewRecovered,
-				"TotalRecovered": response.data.Global.TotalRecovered,
-				"Date": response.data.Global.Date,
-			};
-			$scope.showGlobal();
-			$scope.data = response.data;
-			}, function (error) {
-				console.log(error.data)
-			});
-	}
-	$scope.getSummary();
+    $scope.getSummary = function(){
+        $http({
+            method: 'GET',
+            url: 'https://api.covid19api.com/summary'
+        }).then(function (response) {
+            $scope.Global = {
+                "ID": response.data.ID,
+                "Country": "Worldwide",
+                "CountryCode": "WW",
+                "Slug": "world",
+                "NewConfirmed": response.data.Global.NewConfirmed,
+                "TotalConfirmed": response.data.Global.TotalConfirmed,
+                "NewDeaths": response.data.Global.NewDeaths,
+                "TotalDeaths": response.data.Global.TotalDeaths,
+                "NewRecovered": response.data.Global.NewRecovered,
+                "TotalRecovered": response.data.Global.TotalRecovered,
+                "Date": response.data.Global.Date,
+            };
+            $scope.showGlobal();
+            $scope.data = response.data;
+        }, function (error) {
+            console.log(error.data)
+        });
+    }
+    $scope.getSummary();
 }])
--- js/controllers/MainController.spec.js
+++ js/controllers/MainController.spec.js
@@ -1,60 +1,60 @@
 describe('MainController', function() {
-	beforeEach(module('covid19'));
-	
-	var $scope, $controller, $rootScope, $http, $httpBackend;
-	
-	beforeEach(inject(function(_$controller_, _$rootScope_, _$http_,  _$httpBackend_){
-		$rootScope = _$rootScope_;
-		$scope = $rootScope.$new();
-		$controller = _$controller_;
-		controller = $controller('MainController', { $scope: $scope });
-		$http = _$http_;
-		$httpBackend = _$httpBackend_;
-	}));
+    beforeEach(module('covid19'));
+    
+    var $scope, $controller, $rootScope, $http, $httpBackend;
+    
+    beforeEach(inject(function(_$controller_, _$rootScope_, _$http_,  _$httpBackend_){
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+        $controller = _$controller_;
+        controller = $controller('MainController', { $scope: $scope });
+        $http = _$http_;
+        $httpBackend = _$httpBackend_;
+    }));
 
-	describe('$scope.showCountry', function() {
-		it('sets $scope.country to the country with the matching slug', function() {
-			$scope.data = {};
-			$scope.data.Countries = [
-			{"Country": "Italy","Slug": "italy"},
-			{"Country": "United States of America","Slug": 'united-states'}
-			];
-			$scope.showCountry('united-states');
-			expect($scope.country.Country).toEqual("United States of America");
-		});
-	});
-	
-	describe('$scope.showGlobal', function() {
-		it('sets $scope.country to the global information in $scope.Global', function() {
-			$scope.Global = {
-				"Country": "Worldwide",
-				"Slug": "world"
-			};
-			$scope.showGlobal();
-			expect($scope.country.Country).toEqual("Worldwide");
-			expect($scope.country.Slug).toEqual("world");
-		});
-	});
+    describe('$scope.showCountry', function() {
+        it('sets $scope.country to the country with the matching slug', function() {
+            $scope.data = {};
+            $scope.data.Countries = [
+            {"Country": "Italy","Slug": "italy"},
+            {"Country": "United States of America","Slug": 'united-states'}
+            ];
+            $scope.showCountry('united-states');
+            expect($scope.country.Country).toEqual("United States of America");
+        });
+    });
+    
+    describe('$scope.showGlobal', function() {
+        it('sets $scope.country to the global information in $scope.Global', function() {
+            $scope.Global = {
+                "Country": "Worldwide",
+                "Slug": "world"
+            };
+            $scope.showGlobal();
+            expect($scope.country.Country).toEqual("Worldwide");
+            expect($scope.country.Slug).toEqual("world");
+        });
+    });
 
-	describe('$scope.getSummary', function() {
-		it('should make an $http GET call to the url for getting a summary by default', function() {
-			var global = {
-				"NewConfirmed": 10,
-				"TotalConfirmed": 100,
-				"NewDeaths": 20,
-				"TotalDeaths": 200,
-				"NewRecovered": 30,
-				"TotalRecovered": 300,
-				"Date": (new Date()).toISOString()
-				};
-			$scope.getSummary();
-			var response = {ID:1, Global: global};
-			$httpBackend.when('GET', 'https://api.covid19api.com/summary').respond(response);
-			$httpBackend.expect('GET', 'https://api.covid19api.com/summary');
-			$httpBackend.expect('GET', 'https://api.covid19api.com/summary');
-			$httpBackend.flush();
-			$httpBackend.verifyNoOutstandingExpectation();
-			$httpBackend.verifyNoOutstandingRequest();
-		});
-	});
+    describe('$scope.getSummary', function() {
+        it('should make an $http GET call to the url for getting a summary by default', function() {
+            var global = {
+                "NewConfirmed": 10,
+                "TotalConfirmed": 100,
+                "NewDeaths": 20,
+                "TotalDeaths": 200,
+                "NewRecovered": 30,
+                "TotalRecovered": 300,
+                "Date": (new Date()).toISOString()
+            };
+            $scope.getSummary();
+            var response = {ID:1, Global: global};
+            $httpBackend.when('GET', 'https://api.covid19api.com/summary').respond(response);
+            $httpBackend.expect('GET', 'https://api.covid19api.com/summary');
+            $httpBackend.expect('GET', 'https://api.covid19api.com/summary');
+            $httpBackend.flush();
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+    });
 });
